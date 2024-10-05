f4823701d86407dada8b954d4eda73545a7e29f6
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-05 23:38:26 +0500

    Added Link For Sending Data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 interface Memes {
@@ -14,15 +15,22 @@ export default async function Home() {
 
   return (
     <>
-      <h1>Meme Generator</h1>
+      <h1 className="text-3xl underline text-center mt-4 mb-8">Meme Generator</h1>
       <div className="flex justify-center gap-5 flex-wrap">
-          {
-            response && response.data.memes.map((item: Memes) => {
-              return <div key={item.id}>
-                <Image src={item.url} alt="Memes" width={200} height={200} />
-              </div>
-            })
-          }
+        {
+          response && response.data.memes.map((item: Memes) => {
+            return <div key={item.id}>
+              <Image src={item.url} alt="Memes" width={200} height={200} />
+              <button className="btn my-2 border border-[black] p-2"><Link href={{
+                pathname: "creatememe",
+                query: {
+                  url: item.url,
+                  id: item.id,
+                }
+              }}>Generate Meme</Link></button>
+            </div>
+          })
+        }
       </div>
     </>
   );
