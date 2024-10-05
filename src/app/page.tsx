5cd03ee042105b6a11db8c2ea34db3231dfd7a8c
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-05 23:05:10 +0500

    Fetch The Meme Api And Render Image On The Screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Image from "next/image";
+import React from "react";
 
-export default function Home() {
+interface Memes {
+  id: number;
+  name: string;
+  url: string;
+}
+
+export default async function Home() {
+  const data = await fetch("https://api.imgflip.com/get_memes");
+  const response = await data.json();
+  console.log(response.data.memes);
+
   return (
     <>
-    <h1>Project Initialized</h1>
+      <h1>Meme Generator</h1>
+      <div className="flex justify-center gap-5 flex-wrap">
+          {
+            response && response.data.memes.map((item: Memes) => {
+              return <div key={item.id}>
+                <Image src={item.url} alt="Memes" width={200} height={200} />
+              </div>
+            })
+          }
+      </div>
     </>
   );
 }
