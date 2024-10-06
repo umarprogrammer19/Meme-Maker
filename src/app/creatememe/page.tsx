6e4f535101414edba8366fbcf59fcb9129265184
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-06 14:40:26 +0500

    Get Data By SearchParams

--- src/app/creatememe/page.tsx
+++ src/app/creatememe/page.tsx
@@ -1,9 +1,19 @@
+"use client";
+import Image from 'next/image';
 import React from 'react';
 
-const CreateMeme = () => {
+interface Meme {
+    id: string;
+    url: string;
+}
+
+const CreateMeme = async ({ searchParams }: { searchParams: Meme }) => {
     return (
-        <div>Create Meme</div>
+        <>
+            <div>Create Meme</div>
+            <Image src={searchParams.url} alt='Meme' width={200} height={200} />
+        </>
     )
 }
 
-export default CreateMeme
+export default CreateMeme;
